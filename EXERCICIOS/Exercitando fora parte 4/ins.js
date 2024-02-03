2f40099ecf5afbc813ccef8461a2475ff093e96c
LeonardoHenrique <154704500+LeonardoHenriqueDC@users.noreply.github.com>
2024-02-02 23:29:18 -0400

    Treinando

---
+++ EXERCICIOS/Exercitando fora parte 4/ins.js
@@ -0,0 +1,13 @@
+// criando as variaveis 
+var nome = document.querySelector('#nome')
+var idade = document.querySelector('#idade')
+var op1 = document.querySelector('#op1')
+var op2 = document.querySelector('#op2')
+var op3 = document.querySelector('#op3')
+var op4 = document.querySelector('#op4')
+var op4 = document.querySelector('#op4')
+var submit = document.querySelector('#Enviar')
+var res1 = document.querySelector('#res1')
+var res = document.querySelector('#res')
+
+// criando as funções 
