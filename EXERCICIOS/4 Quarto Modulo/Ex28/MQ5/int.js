47db5ee45b986e8d18868baa52ac54f07f679629
LeonardoHenrique <154704500+LeonardoHenriqueDC@users.noreply.github.com>
2024-02-17 18:59:04 -0400

    MQ5.2

---
+++ EXERCICIOS/4 Quarto Modulo/Ex28/MQ5/int.js
@@ -0,0 +1,33 @@
+// criando as variaveis
+
+let burguer = document.querySelector('#burguer');
+let menu = document.querySelector('#menu');
+let body = document.querySelector('#body');
+
+
+//Escuta 1
+function click(){
+    
+    if(menu.style.display === 'block'){
+        menu.style.display = 'none'
+    }
+    else if(menu.style.display = 'none'){
+        menu.style.display = 'block'
+    }
+}
+
+function redm(){
+    if(window.innerWidth >= 768){
+        menu.style.display = 'block'
+    }
+    else{
+        menu.style.display = 'none'
+    }
+}
+
+
+// Chamada Escuta 1
+burguer.addEventListener('click', click)
+
+// Chamada Escuta 2
+window.addEventListener('resize', redm)
